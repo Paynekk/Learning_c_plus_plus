242bdb3a0c306221b30dacc0b3851a7dcae55130
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-02 01:59:42 -0500

    Ejemplo de while con pregunta

---
+++ fundamentos/While.cpp
@@ -0,0 +1,20 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+string respuesta;
+
+cout << "Quieres la pastilla roja?";
+cin >> respuesta;
+
+while(respuesta == "no"){
+cout << "Estas seguro que no la quieres?";
+cin >> respuesta;
+// final s
+}
+cout << "Bienvenido al mundo REAAAAL!!" << endl;
+}
