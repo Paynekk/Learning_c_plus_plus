e9667bd07cfac7bf0b706268de884f98f4ea8f6f
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-02 17:48:20 -0500

    Ejemplo de un while a for

---
+++ fundamentos/While_a_For.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int i;
+    for (i = 0; i < 26; i++){
+
+        cout << char (i + 97);
+
+    }
+    cout << endl;
+}
+
+
+
+
+
+/*
+While:
+
+    int i = 0;
+    i = 0;     Inicializacio
+
+    while (1 < 26){   Condicion
+
+        cout << char(i + 97);
+
+        i = i + 1     incremento
+    }
+    cout << endl;
+*/
