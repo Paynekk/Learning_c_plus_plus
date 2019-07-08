a9c91194036ceb3ba46aaf650541d2acacd1670d
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-08 11:32:03 -0500

    Utilizando una libreria para obtener la longitud de un vector

---
+++ fundamentos/7_funciones/longitudVector.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+#include <cmath> // libreria math
+
+using namespace std;
+
+int main(){
+double x, y;
+cin >> x >> y;
+cout << sqrt (pow(x , 2.0) + pow(y, 2.0) ) << endl;
+}
