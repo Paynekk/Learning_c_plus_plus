e094fd051f5b45d1d80c56e27747fe561d429511
Miguel <kemse01@gmail.com>
2019-07-13 23:51:57 -0500

    Getline condicion

---
+++ Ejercicios/2_Getline_y_repaso/Acrostico.cpp
@@ -0,0 +1,19 @@
+/*Determinar la salida del programa siguiente:
+Artistically I pen my verse
+Crafting as I go
+Romancing every line
+Ordering the flow
+Systematically perfected,
+The verses are refined
+In every work I labor
+Concerned with what's defined
+---
+*/
+#include <iostream>
+using namespace std;
+int main() {
+
+
+    cin >> x >> endl;
+    cout << x << endl;
+}
