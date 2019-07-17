140ffd4392771d6e60b5896018cd7e813bca645b
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-17 18:30:53 -0500

    fibonacci suma del numero anterior al numero siguiente

--- fundamentos/10_secuencias/1_esquema_recorrido/1_fibonacci.cpp
+++ fundamentos/10_secuencias/1_esquema_recorrido/1_fibonacci.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 int main(){
   int N, n = 1, ultn = 0;
-  cin >> N;
-  cout << ultn << ' ';
-  N--;
+  cin >> N; // 5 
+  cout << ultn << ' '; // << 0
+  
     while(N > 0){
-      cout << n << ' ';
+      cout << n << ' '; // muestra por pantalla n, que despues de la primer entrada es la suma del 
+                         // numero anterior y numero siguiente
 
-      int aux = n + ultn;
-      ultn = n;
-      n = aux;
-      N--;
+      int aux = n + ultn;  //  suma numero anterior  y  numero siguiente
+      ultn = n;  //  numero anterior es  ahora el numero siguiente
+      n = aux; //  n es la suma del numero anterior y numero siguiente 
+      N--; // 4  la secuensia se va a cabando -1 vada vez que pase por aqui
   }
   cout << n << endl;
 }
