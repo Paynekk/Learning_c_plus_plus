92b45df6ce31048f1e69418f0d83562ca652e0b6
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-19 12:20:55 -0500

    secuela sin centinela muuy basica

---
+++ 11_secuencias_sin_centinela/el_cin_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = -17;
+  if(cin >> a){
+    cout << "Es un entero, i es" << a << endl;
+  }else{
+    cout << "No es un eterno" << endl;
+  }
+}
