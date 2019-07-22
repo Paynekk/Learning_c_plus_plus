bfa1d7307d82c38847ca896e4d41cab6e4a14b3c
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-22 09:12:50 -0500

    testing de unidades.cpp para entender el array de strings

---
+++ Ejercicios/5_secuencias_sin_centinela/1_determinar_salida/while_cin/9_unidadestest.cpp
@@ -0,0 +1,21 @@
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   string s, t, p;
+   int p2;
+cin >> s;
+p2 = s.size();
+
+cout << p2 << " "<< s[1]<< endl;
+   /* while (cin >> s) { // se inserta un entero
+  p = s.size()
+      if (s[0] == '-') {// si es un numero con un menos(negativo)
+         t += s[s.size()-1];//    -->
+         cout << t << endl;//     -->
+      }
+   } */
+}
