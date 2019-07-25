36096e2c646583eb069ba15de3aef432d2329d8c
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-24 22:45:58 -0500

    testeo para convertir un numero a negativo

---
+++ Ejercicios/5_secuencias_sin_centinela/3_programas_sin_centinela/3_test.cpp
@@ -0,0 +1,21 @@
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+void test(){
+  int resultado;
+  char resta = '-';
+  int numero = 5;
+  resultado = -numero;
+  cout  << resultado << endl;
+  
+}
+
+
+int main(){
+test();
+}
