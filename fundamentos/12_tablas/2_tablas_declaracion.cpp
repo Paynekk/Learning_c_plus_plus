00e7ca080096e9deea61626333c44462ce639219
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-25 18:52:18 -0500

    como declarar las tablas

---
+++ fundamentos/12_tablas/2_tablas_declaracion.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i , t1[3] ,j ,t2[4];//tambien pueden convinarse las tablas con otro tipo de variables como en esta linea y abajo
+  int T[] = {1,0,1,0,1,0,1,0}, k; // cuando no especificas el numero de espacios
+                              // en automatico los asigna
+  
+  cout << T[8] << endl;
+}
