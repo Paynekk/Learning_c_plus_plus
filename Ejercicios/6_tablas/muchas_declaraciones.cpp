63e3237fbcad3ca416a2006aaf2b4dd9853311be
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-25 19:18:18 -0500

    declarando muchas tabla

---
+++ Ejercicios/6_tablas/muchas_declaraciones.cpp
@@ -0,0 +1,17 @@
+/*
+Muchas Declaraciones 
+Escribe al declaración de:
+
+Una tabla de 50 chars.
+Una tabla de 100 reales.
+Una tabla de 2 strings.
+Una tabla de 1 float (qué utilidad puede tener una tabla de una casilla?).
+ */
+#include <iostream>
+using namespace std;
+int main(){
+  char caracteres[50];
+  double caracteres[100];
+  string caracteres[2];
+  float caracteres[1];
+}
