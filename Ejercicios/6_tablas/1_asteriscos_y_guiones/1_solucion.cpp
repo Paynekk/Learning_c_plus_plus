de45b999925b9ea85d95973da01ab53d3e447a99
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-26 13:11:53 -0500

    solucion a la salida *-*-*-*-

---
+++ Ejercicios/6_tablas/1_asteriscos_y_guiones/1_solucion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int i = 0;
+   bool R[40];
+   // rellenar
+    for (int k = 0; k < 40;k ++){
+      if(k % 2 == 0){
+         R[k] = true;
+      }else{              
+         R[k] = false;
+      }
+    }
+   // mostrarC
+    for (int k = 0; k < 40; k++) {
+      if (R[k]) cout << '*';
+      else cout << '-';
+   } 
+};
+
+
+
+
+
+
+
+
+/* -----------------------
+2.-           k = k + 4
+      R[k]= false; R[k+1] = false;
+      R[k+2]= true; R[k+3] = true;  
+ */
