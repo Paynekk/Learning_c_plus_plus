1fd2821238d333441d17c6022e2a8f5303407493
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-25 23:39:47 -0500

    acceso a las casillas

---
+++ fundamentos/12_tablas/3_acceso_a_casillas.cpp
@@ -0,0 +1,14 @@
+/*
+Acceso a las Casillas 
+Para acceder a una casilla de una tabla se utiliza el nombre de la tabla i después entre corchetes el índice de la casilla que queremos utilizar. Cada casilla es una variable así que trataremos las casillas como si fueran variables con un nombre raro. Esto nos permite hacer cualquier cosa que sepamos hacer con variables con las casillas de una tabla.
+
+Por ejemplo, las siguientes instrucciones declaran una tabla de 2 caracteres y la llenan con 2 valores:
+ */
+
+#include <iostream>
+using namespace std;
+int main(){
+  char grupo[2];
+grupo[0] = 'U';
+grupo[1] = '2';
+}
