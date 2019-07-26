40949002d26778887165f6581eab13a2b0708093
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-25 23:36:50 -0500

    declarando tabla con 5 enteros

--- Ejercicios/6_tablas/2_los_5.cpp
+++ Ejercicios/6_tablas/2_los_5.cpp
@@ -6,5 +6,5 @@ Declara una tabla de 5 enteros y llénala con los valores 1, 2, 3, 4 y 5.
 #include <iostream>
 using namespace std;
 int main(){
-  int cinco[]{1,2,3,4,5};
+  int cinco[5]{1,2,3,4,5};
 }
