ef370ffdd8fb656bf81c53c1b6e71bb2f84f590a
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-29 18:02:12 -0500

    solucion  a la salida solucion a la salida *-*-*-*-

--- Ejercicios/6_tablas/1_asteriscos_y_guiones/1_solucion.cpp
+++ Ejercicios/6_tablas/1_asteriscos_y_guiones/1_solucion.cpp
@@ -4,7 +4,7 @@ int main() {
   int i = 0;
    bool R[40];
    // rellenar
-    for (int k = 0; k < 40;k ++){
+    for (int k = 0; k < 40;k++){
       if(k % 2 == 0){
          R[k] = true;
       }else{              
