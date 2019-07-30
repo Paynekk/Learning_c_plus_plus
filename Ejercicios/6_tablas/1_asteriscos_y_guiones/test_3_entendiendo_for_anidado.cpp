53519370715192709a268e1a2e1f2ea2c1436c2c
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-30 14:53:41 -0500

    entendiendo el for anidado, para el ejercicio 3

---
+++ Ejercicios/6_tablas/1_asteriscos_y_guiones/test_3_entendiendo_for_anidado.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+#include <iostream>
+using namespace std;
+ int main(){
+   for(int i=0,k=0; i<8;i++){
+
+     for(int r=0;r<i;r++, k++){
+       cout << i << endl;
+        cout << k << endl;
+     }
+     k++;
+  cout << k << " " ;
+   }
+ }
