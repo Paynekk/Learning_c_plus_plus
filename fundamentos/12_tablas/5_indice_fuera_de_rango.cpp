5fd940517ae9be55b0f84b55d12033523e8983d9
Miguel Angel Preciado <kemse01@gmail.com>
2019-07-30 18:15:33 -0500

    como NO usar el indice de una tabla

---
+++ fundamentos/12_tablas/5_indice_fuera_de_rango.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int n = 14;
+  char T[80];
+  T[80] = '!'; // las casillas de una tabla SIEMPRE empiezan desde 0 en este caso,
+  cout << n << endl;// del 0 al 79 son 80 claramente.  si te pasas puedes afectar las variables declaradas o crashear el programa
+
+}
