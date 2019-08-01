1875d8c677293cf865e705fe5a0e01dd90f58307
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-01 12:09:01 -0500

    declarando typedef para tablas

---
+++ fundamentos/12_tablas/1_typedef_para_tablas/1_typedef.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+typedef int Numeros[100];
+
+int main(){
+  Numeros t, s;
+  /*
+  otra forma seria la siguiente:
+  int t[100], s[100];
+   */
+  t[0] = 1;
+  s[3] = -3;
+  cout << t[0] << ' ' << s[3] << endl;
+}
