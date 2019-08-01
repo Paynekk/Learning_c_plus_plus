9fb65a25ad4f2d440c283c9d59110250ea3eafd4
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-01 12:32:53 -0500

    utilizar typedef para declarar tablas.

--- fundamentos/12_tablas/1_typedef_para_tablas/1_typedef.cpp
+++ fundamentos/12_tablas/1_typedef_para_tablas/1_typedef.cpp
@@ -5,11 +5,11 @@ typedef int Numeros[100];
 
 int main(){
   Numeros t, s;
+  t[0] = 1;
+  s[3] = -3;
+  cout << t[0] << ' ' << s[3] << endl;
   /*
   otra forma seria la siguiente:
   int t[100], s[100];
    */
-  t[0] = 1;
-  s[3] = -3;
-  cout << t[0] << ' ' << s[3] << endl;
 }
