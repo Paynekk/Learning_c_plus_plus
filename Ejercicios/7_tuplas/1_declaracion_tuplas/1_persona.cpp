40e9dd3a30b1c339871feee7549e5020753c6915
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-06 15:05:07 -0500

    delcarando una tupla 'persona'

---
+++ Ejercicios/7_tuplas/1_declaracion_tuplas/1_persona.cpp
@@ -0,0 +1,17 @@
+/*
+Persona
+Declara una tupla Usuario con los campos siguientes:
+el nombre y dos apellidos (todos strings), la edad (un entero),
+el teléfono (un entero), y si es un usuario administrador (un Booleano).
+ */
+
+#include <iostream>
+using namespace std;
+struct Usuario{
+  string nombre, apellido_p, apellido_m;
+  int edad, telefono;
+  bool administrador;
+};
+int main(){
+
+}
