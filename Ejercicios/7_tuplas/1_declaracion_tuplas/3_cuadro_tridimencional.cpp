e440d7bf1445f995659e07dbfdb3ba00e1b0543b
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-06 15:47:00 -0500

    declarando tupla con datos cordenadas de un punto tridimensional

---
+++ Ejercicios/7_tuplas/1_declaracion_tuplas/3_cuadro_tridimencional.cpp
@@ -0,0 +1,14 @@
+/*
+Punto Tridimensional
+Declara una tupla Punto3D con 3 reales que son las coordenadas x, y y z.
+ */
+
+#include <iostream>
+using namespace std;
+
+struct Punto3D{
+  double x, y, z; 
+};
+int main(){
+
+}
