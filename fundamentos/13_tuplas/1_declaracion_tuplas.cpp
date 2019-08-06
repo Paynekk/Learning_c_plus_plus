f78db3d6a9799b4294511ac7c54e01af1668e970
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-06 14:49:08 -0500

    como declarar una tupla

---
+++ fundamentos/13_tuplas/1_declaracion_tuplas.cpp
@@ -0,0 +1,22 @@
+
+
+/*
+
+COMO DECLARAR UNA TUPLA
+ */
+#include <iostream>
+using namespace std;
+int main(){
+//palabra reservada           nombre del tipo
+struct                         Fecha{
+  int dia, mes, anyo; // Campos(fields)
+/*
+o igual
+
+int dia;
+int mes;
+int anyo;
+ */
+}; // <--- importante cerrar con punto y coma
+
+}
