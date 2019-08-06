831a61e6f95f3051e6ab3baa0ea73d51b5532c76
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-06 15:46:17 -0500

    delcarando una tupla con campos de una 'imagen'

---
+++ Ejercicios/7_tuplas/1_declaracion_tuplas/2_imagen.cpp
@@ -0,0 +1,20 @@
+/*
+Imagen
+Declara una tupla Imagen con los campos siguientes:
+el nombre del fichero donde está la imagen (un string), 
+el ancho y alto (dos enteros), una descripcion (un string)
+y si es en blanco y negro (un Booleano).
+ */
+
+
+#include <iostream>
+using namespace std;
+struct Imagen{
+ string  nombre_fichero;
+ int ancho, alto;
+ string descripcion;
+ bool color;
+};
+int main (){
+
+}
