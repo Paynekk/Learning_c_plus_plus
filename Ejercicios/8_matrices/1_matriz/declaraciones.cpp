796e48e70087fb813b51b8cee461f13c0a67e14a
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-12 11:03:35 -0500

    declaraciones directas y utilizando typedef

---
+++ Ejercicios/8_matrices/1_matriz/declaraciones.cpp
@@ -0,0 +1,21 @@
+/*
+ Declaraciones
+
+  Declara una matriz de forma directa y también utilizando typedef para los siguientes tipos:
+
+    Una matriz de 20 filas y 40 columnas de caracteres.
+    Una matriz de 3 filas y 3 columnas de reales.
+    Una matriz de 12 filas y 31 columnas de un tipo DiaSemana (puedes suponer que el tipo ya existe).
+*/
+
+#include <iostream>
+using namespace std;
+
+typedef double Reales[3][3];
+
+int main(){
+  char caracteres[20][40];
+  int numero[12][31];
+  Reales hola;
+
+}
