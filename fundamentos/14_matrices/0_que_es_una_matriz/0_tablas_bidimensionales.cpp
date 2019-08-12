c8ba7fe4d926a6aabfed06be7ed077c52cd5b12e
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-12 10:43:31 -0500

    Que es una matriz? llenando con doble for

---
+++ fundamentos/14_matrices/0_que_es_una_matriz/0_tablas_bidimensionales.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// tabla, arreglo. (arrays)
+// bidimensionales
+//http://www.minidosis.org/#/actividades/Cpp.Matrices.MatricesTablasBidimensionales.vid
+
+int main(){
+  int i, j;
+  int M[40][30];
+  for(i=0; i < 40; i++){
+    for(j=0;j < 30; j++){
+      M[i][j] = 0;
+    }
+  }
+}
