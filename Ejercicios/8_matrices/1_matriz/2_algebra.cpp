dd1213b8fe5eebdd432acd5f2f115a69f1841d5b
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-12 17:15:48 -0500

    Declara una tabla de 10 matrices de 3 por 3 reales

---
+++ Ejercicios/8_matrices/1_matriz/2_algebra.cpp
@@ -0,0 +1,18 @@
+/*  Álgebra
+
+Declara una tabla de 10 matrices de 3 por 3 reales.
+ */
+typedef double Matriz[3][3];
+
+
+
+/*inse*/
+#include <iostream>
+using namespace std;
+int main(){
+  Matriz tabla[10];
+  
+  cout << tabla[10][3][3] << endl;
+  
+}
+
