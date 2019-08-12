2cf27b51bd19477dac39015ed602bc1442edb996
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-12 16:51:26 -0500

    un tipo que sea una matriz y cada espacio de la matriz sea una tupla

--- Ejercicios/8_matrices/1_matriz/1_RGB.cpp
+++ Ejercicios/8_matrices/1_matriz/1_RGB.cpp
@@ -4,20 +4,18 @@
   Declara un tipo ImagenRGB que sea una matriz de 640 filas y 480 columnas de píxeles,
    donde cada píxel es una tupla Pixel con 3 componentes enteras r, g y b.
 */
-
-typedef int ImagenRGB[2][2];
 struct RGB{
   int r,g,b;
 };
+typedef RGB ImagenRGB[2][2];
+
 
 #include <iostream>
 using namespace std;
 int main (){
-  RGB ImagenRGB[2][2];
-  ImagenRGB[0][0].r = 1;
-  ImagenRGB[0][0].g = 2;
-  ImagenRGB[0][0].b = 3;
-  cout << ImagenRGB[0][0].b << endl; 
-
-
+  ImagenRGB hola;
+  hola[0][0].r = 1;
+  hola[0][0].g = 2;
+  hola[0][0].b = 3;
+  cout << hola[0][0].b << endl; 
 }
