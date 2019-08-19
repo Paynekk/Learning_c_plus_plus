a0cfdc3872d1222a0e3a401e01219346a7278ded
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-18 21:27:41 -0500

    Define una estructura de datos para almacenar un polígono de 7 lados

---
+++ Ejercicios/9_tipos_estructurados/2_definir_tabla_con_casillas_tipo_tupla/poligono.cpp
@@ -0,0 +1,16 @@
+/*
+ Polígono
+
+Define una estructura de datos para almacenar un polígono de 7 lados en el plano real.
+*/
+#include <iostream>
+using namespace std;
+struct Cordenadas{
+  double x,y;
+
+};
+
+int main(){
+Cordenadas Poligono[7];
+
+}
