aa6cc97fa7e0b56025987d052dee7ceb277b4158
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-18 21:35:34 -0500

    tabla de usuarios con email,fecha alta, etc

---
+++ Ejercicios/9_tipos_estructurados/2_definir_tabla_con_casillas_tipo_tupla/usuarios.cpp
@@ -0,0 +1,20 @@
+/*
+ Usuarios
+  Necesitamos almacenar la tabla de usuarios de Microdosis,
+  un sitio web de aprendizaje online, que tiene exactamente 100 usuarios.
+  Cada usuario tiene un e-mail, la fecha de alta, la fecha del último login,
+  y el tiempo acumulado en minutos que ha pasado conectado trabajando.
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Usuarios{
+string email;
+int FechaInicio, FechaLoing, tiempoConexion;
+};
+
+int main(){
+Usuarios Microdosis[100];
+
+}
