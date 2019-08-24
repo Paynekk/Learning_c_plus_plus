1317899c6ea9e7ce3741b25794a519f34e1a0f82
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-23 23:40:54 -0500

    Por terminar

--- fundamentos/15_tipos_estructurados/5_Tablas_parcialmente_ocupadas/2_borrar_elemento.cpp
+++ fundamentos/15_tipos_estructurados/5_Tablas_parcialmente_ocupadas/2_borrar_elemento.cpp
@@ -15,8 +15,22 @@ void mostrar(const Frase& F){
     cout << F.palabras[i];
   }
 }
-
+void borrar (Frase& F, int pos){
+  if(pos >= F.npalabras){
+    pos = F.npalabras-1;
+  } else if(pos < 0){
+    pos = 0;
+  }
+  int i = pos;
+  while(i < F.npalabras-1){
+    F.palabras[i] = F.palabras[i+1];
+    i++;    
+  }
+  F.npalabras--;
+}
 int main(){
   Frase F = {{"uno","dos","tres","cuatro","cinto"},5};
+  borrar(F,2);
   mostrar(F);
   cout << endl;
+}
