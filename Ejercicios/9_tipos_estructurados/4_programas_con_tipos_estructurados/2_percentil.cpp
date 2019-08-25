8af418e3cbc0d46b29a20070c4754c04ce6e5d1b
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-24 20:39:27 -0500

    primer commit

---
+++ Ejercicios/9_tipos_estructurados/4_programas_con_tipos_estructurados/2_percentil.cpp
@@ -0,0 +1,27 @@
+/*
+Percentil 
+Haz un programa que lee una lista de bebés como la siguiente
+
+Nicoleta 5.95
+Enric 8.1
+Arnau 7.1
+Violeta 6.25
+Duna 7.85
+...
+y escribe a la salida el percentil de cada bebé. El percentil de un bebé 
+es el porcentaje de bebés (incluyéndolo a él mismo) que tienen un peso igual o menor.
+*/
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct Percentil{
+  string nombre;
+   double peso;
+};
+void leer_fichero()
+
+int main(){
+  Percentil bebes[100];
+  
+}
