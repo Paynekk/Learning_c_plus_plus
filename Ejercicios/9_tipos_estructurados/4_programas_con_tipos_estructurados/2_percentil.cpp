55baae141582c89196fe2e53a6d773874db08dd7
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-26 17:46:56 -0500

    percentil de cada bebe

--- Ejercicios/9_tipos_estructurados/4_programas_con_tipos_estructurados/2_percentil.cpp
+++ Ejercicios/9_tipos_estructurados/4_programas_con_tipos_estructurados/2_percentil.cpp
@@ -19,9 +19,36 @@ struct Percentil{
   string nombre;
    double peso;
 };
-void leer_fichero()
+void leer_fichero(Percentil bebes[], int& tamanyo){
+  ifstream F;
+  F.open("2_percentil.txt");
+  while(F >> bebes[tamanyo].nombre >> bebes[tamanyo].peso){
+    tamanyo++;
+  }
+  F.close();
+}
+
+void bebes_percentil(Percentil bebes[], int tamanyo){
+  
+  for(int i=0;i<tamanyo; i++){
+      int conteo=0; double porcentaje;
+      cout << bebes[i].nombre << ", percentil: "; 
+    for (int j = 0; j < tamanyo; j++){
+      if(bebes[j].peso <= bebes[i].peso){
+          conteo++;
+      }
+    }
+     porcentaje = conteo*100/tamanyo;
+     cout << porcentaje << '%' << endl;
+
+  }
+}
 
 int main(){
-  Percentil bebes[100];
+  Percentil bebes[100]; int tamanyo=0;
+  leer_fichero(bebes, tamanyo);
+  bebes_percentil(bebes, tamanyo);
+
+
   
 }
