8a717bac5ba2ea77d88f5cb89167ee5e6680bd0f
Miguel Angel Preciado <kemse01@gmail.com>
2019-08-27 16:31:53 -0500

    leer un fichero

---
+++ Ejercicios/10_ficheros/leer_un_fichero.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main(){
+  //       puede variar el nombre
+  ifstream fin("leer_un_fichero.txt");
+  string nombre;
+  int edad;
+  fin >> nombre >> edad;
+  cout << "tu nombre es: " << nombre 
+        << " y tienes " << edad << " anyos" << endl;
+  
+}
