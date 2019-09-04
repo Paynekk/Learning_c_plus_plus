eea4426c7c4c6272a9161faa16cc5ac3e0e68fb4
Miguel Angel Preciado <kemse01@gmail.com>
2019-09-04 01:34:36 -0500

    programa que escribe un fichero con las tablas 1-10

---
+++ Ejercicios/10_ficheros/3_hacer_programas_que_escriban_ficheros/3_tablas.cpp
@@ -0,0 +1,30 @@
+/* Tablas 
+Haz un programa que escriba un fichero tablas.txt con las tablas de multiplicar del 1 al 10. El formato de una tabla es el siguiente
+
+Tabla del 2
+-----------
+2 * 1 = 2
+2 * 2 = 4
+2 * 3 = 6
+2 * 4 = 8
+2 * 5 = 10
+2 * 6 = 12
+2 * 7 = 14
+2 * 8 = 16
+2 * 10 = 20 */
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(){
+ofstream F("tablas.txt", ofstream::app);
+
+  for (int i = 1; i <= 10; i++){
+    F << "Tabla del " << i << endl
+      << "------------------------" << endl;
+    for (int j = 1; j <= 10; j++){
+      F << i << " * " << j << " = " << i*j << endl;
+    }
+    cout << endl;
+  }
+}
