3891323ad1ea014c30b087678336921ce2fd76d1
Miguel Angel Preciado <kemse01@gmail.com>
2019-09-07 22:40:46 -0500

    que es la clase, objeto y metodo

---
+++ fundamentos/17_clase_objeto_y_metodo_/0_clase_objeto_metodo_que_es?.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a = 1;
+  bool b = false;
+  char c = 'z';
+  float d = 0.3;
+  double e = 14.9;
+  long n = 1254367587;
+//tipo nombre
+//tipos integrados de c++
+//----------------------------
+
+// libreria estandar de std
+
+/*clase -->*/  string /*objeto -->*/s;
+  string r;
+  string t;
+  /*metodo = funcion de una clase, que se llama sobre un objeto*/
+  cin >> s >> r;
+  cout << s.size() /*<-- metodo*/<< ' ' << r.size() << endl;
+  cout << t.size() << endl;
+  
+}
