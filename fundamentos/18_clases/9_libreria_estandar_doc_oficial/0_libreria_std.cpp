79342deecd69b880c2322e9894f09b15f05112d9
Miguel Angel Preciado <kemse01@gmail.com>
2019-09-17 17:28:49 -0500

    libreria standar ofcicial de c++

---
+++ fundamentos/18_clases/9_libreria_estandar_doc_oficial/0_libreria_std.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // implica <string>
+                    // declarados cout, cin , endl
+
+#include <cmath>
+#include <fstream> /* 1- se va a buscar el fichero 'fstrea,'
+                     2- se 'incrusta' el contenido del fichero en el programa
+                       implica que las declaraciones que hay en el fichero
+                       estan en el programa
+*/
+using namespace std;
+
+int main(){
+  int a;
+  string s = "hola";
+  a = 1;
+}
